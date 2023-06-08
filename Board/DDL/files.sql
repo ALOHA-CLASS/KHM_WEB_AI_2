addae6815adfdde870ab1ce1fa04f9189bfc867a
wwwalohacampus <h850415@naver.com>
2023-06-08 17:55:10 +0900

    💛

--- Board/DDL/files.sql
+++ Board/DDL/files.sql
@@ -7,9 +7,9 @@ CREATE TABLE files (
     parent_no NUMBER NOT NULL,                  -- 부모 테이블 번호
     file_size NUMBER DEFAULT 0 NOT NULL ,       -- 파일 크기
     reg_date DATE DEFAULT sysdate NOT NULL ,    -- 등록 일자
-    upd_date DATE DEFAULT sysdate NOT NULL      -- 수정 일자
+    upd_date DATE DEFAULT sysdate NOT NULL ,    -- 수정 일자
+    file_code NUMBER DEFAULT 0 NOT NULL         -- 파일 코드
 );
-
 
 -- 파일 테이블 (MySQL)
 CREATE TABLE `files` (
@@ -21,5 +21,6 @@ CREATE TABLE `files` (
   `file_size` int NOT NULL DEFAULT '0',
   `reg_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `upd_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `file_code` int NOT NULL DEFAULT '0',
   PRIMARY KEY (`file_no`)
 ) COMMENT='파일';
